266053a192c243e9b24985ec1c150d8d0f8428b8
Your Name <you@example.com>
2023-10-11 05:46:18 -0600

    add content type for upload_assets

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -41,6 +41,7 @@ resource "aws_s3_object" "upload_assets" {
   bucket   = aws_s3_bucket.website_bucket.bucket
   key      = "assets/${each.key}"
   source   = "${var.assets_path}/${each.key}"
+  content_type = endswith("${var.assets_path}/${each.key}", "svg") ? "image/svg+xml" : "binary/octet-stream" 
   etag     = filemd5("${var.assets_path}${each.key}")
   lifecycle {
     replace_triggered_by = [terraform_data.content_version.output]
