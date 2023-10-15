bcaf98487534546d243fdf1994aeb175910245a1
tfbootcamp23 <145222646+tfbootcamp23@users.noreply.github.com>
2023-10-14 19:50:20 -0600

    41 terratowns provider (#42)
    * add docs on Fixing Tags
    
    * success ./bin/build_provider

--- main.tf
+++ main.tf
@@ -1,12 +1,24 @@
 terraform {
+  required_providers {
+    terratowns = {
+      source  = "local.providers/local/terratowns"
+      version = "1.0.0"
+    }
+  }
 }
 
-module "terrahouse_aws" {
-  source              = "./modules/terrahouse_aws"
-  user_uuid           = var.user_uuid
-  bucket_name         = var.bucket_name
-  index_html_filepath = var.index_html_filepath
-  error_html_filepath = var.error_html_filepath
-  assets_path         = var.assets_path
-  content_version     = var.content_version
+provider "terratowns" {
+  endpoint  = "http://localhost:4567"
+  user_uuid = "6129a7fb-2b48-4581-9351-8c769dad234d"
+  token     = "0ae8527b-067d-480f-ad05-faa278abbab3"
 }
+
+# module "terrahouse_aws" {
+#   source              = "./modules/terrahouse_aws"
+#   user_uuid           = var.user_uuid
+#   bucket_name         = var.bucket_name
+#   index_html_filepath = var.index_html_filepath
+#   error_html_filepath = var.error_html_filepath
+#   assets_path         = var.assets_path
+#   content_version     = var.content_version
+# }
